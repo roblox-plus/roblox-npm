b740e10c89ba6499b36b9d0eb43ef355d817cbde
WebGL3D <OgreLandBob@gmail.com>
2020-12-13 09:54:53 -0800

    Create robloxHttpClient.js

---
+++ roblox/clients/robloxHttpClient.js
@@ -0,0 +1,20 @@
+import { HttpClient } from "@tix-factory/http";
+import XsrfTokenHandler from "./../handlers/xsrfTokenHandler.js";
+
+const modifyOptions = options => {
+	if (!options) {
+		options = {};
+	}
+
+	if (!options.handlers) {
+		options.handlers = [];
+	}
+
+	options.handlers.push(new XsrfTokenHandler());
+};
+
+export default class extends HttpClient {
+	constructor(options) {
+		super(modifyOptions(options));
+	}
+};
--- roblox/index.js
+++ roblox/index.js
@@ -1,2 +1,6 @@
+// Constants
 export { default as AssetTypesById } from "./constants/assetTypesById.js";
 export { default as AssetTypesByName } from "./constants/assetTypesByName.js";
+
+// Clients
+export { default as RobloxHttpClient } from "./clients/robloxHttpClient.js";
